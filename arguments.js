3b2c4c5f988a89d3d8353c4c1f90ac0914dcfd4e
Meghdad Abbaszadegan <m3ghdad@gmail.com>
2017-11-06 13:53:50 -0800

    Adds myBind function using rest operator

--- arguments.js
+++ arguments.js
@@ -13,3 +13,63 @@ function sum2() {
   }
   return total;
 }
+console.log(sum(1, 2, 3, 4) === 10);
+console.log(sum(1, 2, 3, 4, 5) === 15);
+
+
+// simple myBind with no args
+Function.prototype.myBind = function (ctx) {
+  return () => this.apply(ctx);
+};
+
+Function.prototype.myBindArgs = function (ctx, ...args) {
+  return(...callArgs) => {
+    return this.apply(ctx, args.concat(callArgs));
+  };
+};
+
+Function.prototype.myBindArgs2 = function (ctx, arguments) {
+  return(...callArgs) => {
+    return this.apply(ctx, arguments.concat(callArgs));
+  };
+};
+
+
+class Cat {
+  constructor(name) {
+    this.name = name;
+  }
+
+  says(sound, person) {
+    console.log(`${this.name} says ${sound} to ${person}!`);
+    return true;
+  }
+}
+
+const markov = new Cat("Markov");
+const breakfast = new Cat("Breakfast");
+
+markov.says("meow", "Ned");
+// Markov says meow to Ned!
+// true
+
+// bind time args are "meow" and "Kush", no call time args
+markov.says.myBindArgs(breakfast, "meow", "Kush")();
+// Breakfast says meow to Kush!
+// true
+
+// no bind time args (other than context), call time args are "meow" and "me"
+markov.says.myBindArgs(breakfast)("meow", "a tree");
+// Breakfast says meow to a tree!
+// true
+
+// bind time arg is "meow", call time arg is "Markov"
+markov.says.myBindArgs(breakfast, "meow")("Markov");
+// Breakfast says meow to Markov!
+// true
+
+// no bind time args (other than context), call time args are "meow" and "me"
+const notMarkovSays = markov.says.myBindArgs(breakfast);
+notMarkovSays("meow", "me");
+// Breakfast says meow to me!
+// true
